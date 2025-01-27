fe736f4719437965783a4f08b9079eed97072830
irolram <rrivan@iesrafaelalberti.es>
2025-01-27 14:30:30 +0100

    si

---
+++ src/ConjuntoLibros.kt
@@ -0,0 +1,14 @@
+class ConjuntoLibros(val libros: Array<Int>) {
+
+    companion object{
+        val librosAlmacenados = Array(5) { "" }
+    }
+}
+
+    fun añadirLibro(libro: Array<Int>) {
+
+    }
+
+    fun borrarLibro(libro: Array<Int>) {
+
+    }
---
+++ src/Libro.kt
@@ -0,0 +1,4 @@
+class Libro(val titulo: String, val autor: String, val numeroPaginas: Int, val calificacion: Int) {
+
+
+}
