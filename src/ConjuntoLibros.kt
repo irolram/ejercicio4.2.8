78971b957517ef329c45ebbdc8ad2170037495ce
irolram <rrivan@iesrafaelalberti.es>
2025-01-28 14:35:05 +0100

    commit

--- src/ConjuntoLibros.kt
+++ src/ConjuntoLibros.kt
@@ -1,14 +1,38 @@
-class ConjuntoLibros(val libros: Array<Int>) {
+import ConjuntoLibros.Companion.librosAlmacenados
+
+class ConjuntoLibros(val libros: Array<Libro?>) {
 
     companion object{
         val librosAlmacenados = Array(5) { "" }
+        var indiceActual: Int = 0
+    }
+    fun añadirLibro(libro: Libro) {
+
+        if (indiceActual < librosAlmacenados.size) {
+            libros[indiceActual] = libro
+            indiceActual++
+        }else{
+            println("No hay espacio suficiente")
+        }
+
+    }
+    /*fun borrarLibroAutor(autor: String) {
+        for (i in libros.indices) {
+            if (libros[i] == autor) {
+
+            }
+        }
+    }
+
+     */
+
+    fun mostrarLibros() {
+        println("Libros almacenados:")
+        libros.filterNotNull().forEach {
+            println("${it.titulo} - ${it.autor} - ${it.numeroPaginas} páginas - Calificación: ${it.calificacion}")
+        }
     }
 }
 
-    fun añadirLibro(libro: Array<Int>) {
 
-    }
 
-    fun borrarLibro(libro: Array<Int>) {
-
-    }
--- src/Libro.kt
+++ src/Libro.kt
@@ -1,4 +1,10 @@
-class Libro(val titulo: String, val autor: String, val numeroPaginas: Int, val calificacion: Int) {
+data class Libro(val titulo: String, val autor: String, val numeroPaginas: Int, val calificacion: Int) {
 
+    init {
+        require(calificacion in 0..10) { "La calificación debe estar entre 0 y 10" }
+    }
 
+    override fun toString(): String {
+        return "Libro(título='$titulo', autor='$autor', páginas=$numeroPaginas, calificación=$calificacion)"
+    }
 }
